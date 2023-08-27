fa07db7093557aeb9ff79e6daf4eafaa26904c6f
Diren <diren_d_bharwani_from.tp@tech.gov.sg>
2023-08-27 22:35:38 +0800

    Testing deployment pipeline

---
+++ src/handler.py
@@ -0,0 +1,9 @@
+import requests
+
+
+def hello(event, context):
+    response = requests.get("https://jsonplaceholder.typicode.com/todos/1")
+    res = {"event": event, "output": response.json(), "context": context}
+
+    print(res)
+    return None
