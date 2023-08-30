ce0e4994f1ae40436d8f469f1c012135efccaf70
Diren <diren.bharwani@gmail.com>
2023-08-31 01:14:57 +0800

    Revert code and check if lambda can be correctly invoked

--- src/tg-bot.py
+++ src/tg-bot.py
@@ -15,33 +15,7 @@ import telebot
 
 # ======================================================================================================================
 
-logger = telebot.logger
-telebot.logger.setLevel(logging.INFO)
-
-bot = telebot.TeleBot(os.getenv("TELEGRAM_BOT_TOKEN"), threaded=False)
-
-
-def process_event(event):
-    # Get telegram webhook json from event
-    request_body_dict = json.loads(event["body"])
-    # Parse updates from json
-    update = telebot.types.Update.de_json(request_body_dict)
-    # Run handlers and etc for updates
-    bot.process_new_updates([update])
-
 
 def handler(event, context):
-    # Process event from aws and respond
-    process_event(event)
+    print(os.getenv("TELEGRAM_BOT_TOKEN"))
     return {"statusCode": 200}
-
-
-# Handle '/start' and '/help'
-@bot.message_handler(commands=["start"])
-def send_welcome(message):
-    chat_id = message.chat.id
-
-    bot.reply_to(
-        message,
-        f"Hi {chat_id}!",
-    )
