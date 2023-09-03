dda506fddbef0c84ea3d9283a6f38e66519f9d39
Diren <diren.bharwani@gmail.com>
2023-09-03 12:06:22 +0800

    Added ownership controls for terraform state s3 bucks

--- terraform/modules/terraform_state/s3.tf
+++ terraform/modules/terraform_state/s3.tf
@@ -3,9 +3,18 @@ resource "aws_s3_bucket" "terraform_state_bucket" {
   force_destroy = true
 }
 
+resource "aws_s3_bucket_ownership_controls" "terraform_state_ownership_controls" {
+  bucket = aws_s3_bucket.terraform_state_bucket.id
+  rule {
+    object_ownership = "BucketOwner Preferred"
+  }
+}
+
 resource "aws_s3_bucket_acl" "terraform_state_acl" {
-  bucket  = aws_s3_bucket.terraform_state_bucket.id
-  acl     = "private"
+  depends_on  = [aws_s3_bucket_ownership_controls.terraform_state_ownership_controls]
+
+  bucket      = aws_s3_bucket.terraform_state_bucket.id
+  acl         = "private"
 }
 
 resource "aws_s3_bucket_versioning" "terraform_state_versioning"{
