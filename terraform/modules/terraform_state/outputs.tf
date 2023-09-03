eceb2c4ce68d4e2b7152380c31cf2f9e9bc46d5f
Diren <diren.bharwani@gmail.com>
2023-09-03 11:52:46 +0800

    fixed mismatched s3 references

--- terraform/modules/terraform_state/outputs.tf
+++ terraform/modules/terraform_state/outputs.tf
@@ -1,5 +1,5 @@
 output "se_tg_bot_terraform_state_s3_arn" {
-  value       = aws_s3_bucket.terraform_state.arn
+  value       = aws_s3_bucket.terraform_state_bucket.arn
   description = "The ARN of the provisioned S3 bucket for storing the terraform state"
 }
 
--- terraform/modules/terraform_state/s3.tf
+++ terraform/modules/terraform_state/s3.tf
@@ -4,19 +4,19 @@ resource "aws_s3_bucket" "terraform_state_bucket" {
 }
 
 resource "aws_s3_bucket_acl" "terraform_state_acl" {
-  bucket = aws_s3_bucket.terraform_state.id
+  bucket = aws_s3_bucket.terraform_state_bucket.id
   acl = "private"
 }
 
 resource "aws_s3_bucket_versioning" "terraform_state_versioning"{
-  bucket = aws_s3_bucket.terraform_state.id
+  bucket = aws_s3_bucket.terraform_state_bucket.id
   versioning_configuration {
     status = "Enabled"
   }
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_encryption" {
-  bucket = aws_s3_bucket.terraform_state.id
+  bucket = aws_s3_bucket.terraform_state_bucket.id
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
