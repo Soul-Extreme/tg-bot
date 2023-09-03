418de410d353f95f9a08957e5e27c9c3d697a068
Diren <diren.bharwani@gmail.com>
2023-09-03 11:39:49 +0800

    Removed commented backend to satisfy fmt checkl

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -7,14 +7,6 @@ terraform {
       version = ">= 5.15.0"
     }
   }
-
-#  backend "s3" {
-#    bucket          = "se-tg-bot-terraform-state"
-#    region          = "ap-southeast-1"
-#    key             = "state/terraform.tfstate"  # Path to where the terraform state file will be stored in the S3
-#    encrypt         = true
-#    dynamodb_table  = "terraform-locks"
-#  }
 }
 
 provider "aws" {
