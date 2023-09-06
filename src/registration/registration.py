5e23efb3373866a6c2f0133c9f21fd72d46a3c3b
Diren <diren.bharwani@gmail.com>
2023-09-06 22:52:02 +0800

    Added skeleton registration lambda

---
+++ src/registration/registration.py
@@ -0,0 +1,16 @@
+"""
+File        : registration.py
+Author      : Diren D Bharwani
+Date        : 2023-08-30
+
+Description : Entry point for the Telegram Bot Registration.
+"""
+
+import json
+import os
+
+# ======================================================================================================================
+
+
+def handler(event, context):
+    print(event)
--- terraform/modules/storage/dynamodb.tf
+++ terraform/modules/storage/dynamodb.tf
@@ -11,3 +11,17 @@ resource "aws_dynamodb_table" "personal_particulars_table" {
     type = var.dynamodb_attribute_type_number
   }
 }
+
+# ------------------------------------------------------------
+# Member Profile
+# ------------------------------------------------------------
+resource "aws_dynamodb_table" "member_profile_table" {
+  name         = "member-profile"
+  billing_mode = var.dynamodb_billing_mode
+  hash_key     = var.chat_id_attribute
+
+  attribute {
+    name = var.chat_id_attribute
+    type = var.dynamodb_attribute_type_number
+  }
+}
