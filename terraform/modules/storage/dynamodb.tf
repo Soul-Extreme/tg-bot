ad0c7f41131c210c5a1e0262cbf5e47b1e05d202
Diren Bharwani <diren.bharwani@gmail.com>
2023-09-24 23:43:30 +0800

    Create chat-state DynamoDB table for caching chat state between chats

--- terraform/modules/storage/dynamodb.tf
+++ terraform/modules/storage/dynamodb.tf
@@ -25,3 +25,28 @@ resource "aws_dynamodb_table" "member_profile_table" {
     type = var.dynamodb_attribute_type_number
   }
 }
+
+# ------------------------------------------------------------
+# Chat State
+# ------------------------------------------------------------
+resource "aws_dynamodb_table" "chat_state" {
+  name         = "chat-state"
+  billing_mode = var.dynamodb_billing_mode
+  hash_key     = "command"
+  range_key    = var.chat_id_attribute
+
+  attribute {
+    name = "command"
+    type = var.dynamodb_attribute_type_string
+  }
+
+  attribute {
+    name = var.chat_id_attribute
+    type = var.dynamodb_attribute_type_number
+  }
+
+  ttl {
+    enabled        = true
+    attribute_name = "Time To Live"
+  }
+}
